72abf766a2ecf1c73f91dfe51b62482982e9376d
anaplian <anaplian>
2017-01-29 18:13:45 +0000

    Clean up code and refactor

--- pathfinder.js
+++ pathfinder.js
@@ -1,10 +1,18 @@
 var launchPathfinder = (function(){
-    var CANVAS_WIDTH = 600, CANVAS_HEIGHT = 600; // in px
-    var CELL_SIZE = 30; // in px
-    var DELAY = 40; // in ms
-    var BG_COLOUR = "#dee7e7", CLOSED_COLOUR = "#82bee6", OPEN_COLOUR = "#68b3e5",
-        TEXT_COLOUR = "#fff", PATH_COLOUR = "#192025", OBSTACLE_COLOUR = "#fff",
-        START_COLOUR = "#a4ffba", FINISH_COLOUR = "#a4ffba"; CURRENT_COLOUR = "#f8ff8e";
+    var CANVAS_WIDTH_PX = 600, CANVAS_HEIGHT_PX = 600;
+    var CELL_SIZE_PX = 30; 
+    var DELAY_MS = 40;
+    var BG_COLOUR = "#dee7e7", 
+        CLOSED_COLOUR = "#82bee6",
+        OPEN_COLOUR = "#68b3e5",
+        TEXT_COLOUR = "#fff",
+        PATH_COLOUR = "#192025",
+        OBSTACLE_COLOUR = "#fff",
+        START_COLOUR = "#a4ffba",
+        FINISH_COLOUR = "#a4ffba",
+        CURRENT_COLOUR = "#f8ff8e";
+
+    var SCORE_FONT = "arial 15px";
 
     var TIMER = null;
     var TILE_TYPE = {
@@ -12,66 +20,96 @@ var launchPathfinder = (function(){
         OBSTACLE: 1
     };
 
+    var OBSTACLE_PROBABILITY = 0.05,
+        ADJACENT_OBSTACLE_PROBABILITY = 0.3;
+
     var init = function(canvas){
+        // setup canvas context
+        var cxt;
+        canvas.width = CANVAS_WIDTH_PX;
+        canvas.height = CANVAS_HEIGHT_PX;
+		cxt = canvas.getContext('2d');
+        cxt.fillStyle = BG_COLOUR;
+        cxt.fillRect(0, 0, CANVAS_WIDTH_PX, CANVAS_HEIGHT_PX);
 
-        var cxt = setupCanvas(canvas);
-        var rowLength = Math.floor(CANVAS_WIDTH/CELL_SIZE),
-            colLength = Math.floor(CANVAS_HEIGHT/CELL_SIZE);
-        var grid = createGrid(rowLength, colLength);
-        
-        var startPosition = randomCoords(rowLength, colLength);
-        grid[startPosition.x][startPosition.y] = TILE_TYPE.BLANK;
-        
-        var finishPosition = randomCoords(colLength, colLength);
+        // create the grid representation
+        var numRows = Math.floor(CANVAS_WIDTH_PX/CELL_SIZE_PX),
+            numCols = Math.floor(CANVAS_HEIGHT_PX/CELL_SIZE_PX);
+        var grid = createGrid(numRows, numCols);
+
+        // set a random starting position
+        var startPosition = randomCoords(numRows, numCols);
+
+        // set a random end position
+        var finishPosition = randomCoords(numRows, numCols);
+
+        // ensures the start and end positions are accessible
+        // the pathfinder won't evaluate obstacles
         grid[finishPosition.x][finishPosition.y] = TILE_TYPE.BLANK;
+        grid[startPosition.x][startPosition.y] = TILE_TYPE.BLANK;
 
+        // initialise arrays of evaluated (closed) and unevaluated (open) nodes
         var openNodes = [];
         var closedNodes = [];
 
+        // creates a node at the starting position and adds to open list
         var startNode = createNode(startPosition.x, startPosition.y, null, 0, 0, 0);
         openNodes.push(startNode);
         
+        // start the timer
         if(TIMER) {
             window.clearInterval(TIMER);
         }
 
+        // run the step algorithm at an interval
         TIMER = window.setInterval(function(){
-            var currentNode = aStarStep(grid, openNodes, closedNodes, startPosition, finishPosition, cxt);
-            doPainting(openNodes, closedNodes, startPosition, finishPosition, currentNode, grid, cxt);
-        }, DELAY);
+            var currentNode = aStarStep(grid, openNodes, closedNodes,
+             startPosition, finishPosition, cxt);
+            doPainting(openNodes, closedNodes, 
+             startPosition, finishPosition, currentNode, grid, cxt);
+        }, DELAY_MS);
 
-    }
-
-    function setupCanvas(canvas){
-        var cxt;
-
-        canvas.width = CANVAS_WIDTH;
-        canvas.height = CANVAS_HEIGHT;
-		cxt = canvas.getContext('2d');
-        cxt.fillStyle = BG_COLOUR;
-        cxt.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-        return cxt
-    }
+    };
 
     function createGrid(rowLength, colLength){
         var grid = [];
-        for(var x = 0; x < rowLength; x++){
+        var x, y;
+        for(x = 0; x < rowLength; x++){
             grid[x] = [];
-            for(var y = 0; y < colLength; y++){
+            for(y = 0; y < colLength; y++){
                 grid[x][y] = TILE_TYPE.BLANK;
             }
         }
-        grid = addRandomObstacles(grid);
+
+        // add random obstacles to the grid
+        // first pass places random dots
+        for(x = 0; x < grid.length; x++){
+            for(y = 0; y < grid[0].length; y++){
+                if(Math.random() < OBSTACLE_PROBABILITY && grid[x][y] == TILE_TYPE.BLANK){
+                    grid[x][y] = TILE_TYPE.OBSTACLE;
+                }
+            }
+        }
+
+        // second pass beefs up the dots
+        for(x = 0; x < grid.length; x++){
+            for(y = 0; y < grid[0].length; y++){
+                if(grid[x][y] != TILE_TYPE.OBSTACLE && adjacentToObstacle(x,y, grid)){
+                    if(Math.random() < ADJACENT_OBSTACLE_PROBABILITY && grid[x][y] == TILE_TYPE.BLANK){
+                        grid[x][y] = TILE_TYPE.OBSTACLE;
+                    }
+                }
+            }
+        }
 
         return grid;
     }
 
     function aStarStep(grid, open, closed, start, finish, cxt){
-        if(open.length == 0) return;
+        // if there are no squares left to evaluate, stop looking
+        if(open.length === 0) return;
 
         var currentNode = lowestCostNode(open);
-        //console.log(currentNode.g+"+"+currentNode.h+"="+currentNode.f);
         open.splice(open.indexOf(currentNode), 1);
         closed.push(currentNode);
 
@@ -92,15 +130,18 @@ var launchPathfinder = (function(){
 
         for(var node in neighbours){
             if(neighbours.hasOwnProperty(node)){
-                if(!isInList(closed, neighbours[node]) 
-                && !isObstacle(neighbours[node].x, neighbours[node].y, grid)){
-                        
-                    //calc f,g,h
+                if(!isInList(closed, neighbours[node]) &&
+                 !isObstacle(neighbours[node].x, neighbours[node].y, grid)) {
+                     
+                    // calc f, g & h
+                    // f is the total score
+                    // g is the cost from the start node
+                    // h is the heuristic
                     var f, g, h;
                     if(neighbours[node].parent){
                         g = neighbours[node].parent.g + 1;
                     }
-                    h = manhattanDistance(neighbours[node], finish)
+                    h = manhattanDistance(neighbours[node], finish);
                     f = g+h;
 
                     if(!isInList(open, neighbours[node])){
@@ -124,19 +165,13 @@ var launchPathfinder = (function(){
         }
 
         return currentNode;
-
     }
 
     function manhattanDistance(pointA, pointB){
-        if(pointA.hasOwnProperty('x') && pointB.hasOwnProperty('x')
-        && pointA.hasOwnProperty('y') && pointB.hasOwnProperty('y')){
-            var distance = 0;
-
-            distance += Math.abs(pointB.x - pointA.x);
-            distance += Math.abs(pointB.y - pointA.y);
-            
-            return distance;
-        }
+        var distance = 0;
+        distance += Math.abs(pointB.x - pointA.x);
+        distance += Math.abs(pointB.y - pointA.y);
+        return distance;
     }
 
     /** PAINTING METHODS */
@@ -145,36 +180,31 @@ var launchPathfinder = (function(){
         fillNodes(closed, CLOSED_COLOUR, false, cxt);
         fillNodes(open, OPEN_COLOUR, true, cxt);
         fillNode(currentNode, CURRENT_COLOUR, cxt);
-    
+
         fillPath(closed, finish, PATH_COLOUR, cxt);
         fillObstacles(grid, cxt);
-        fillNode(start, START_COLOUR, cxt);
-        fillNode(finish, FINISH_COLOUR, cxt);
+        fillNode([start], START_COLOUR, false, cxt);
+        fillNode([finish], FINISH_COLOUR, false, cxt);
     }
 
     function fillNodes(nodes, colour, writeScore, cxt){
         cxt.fillStyle = colour;
+
+        var i, thisNode;
         if(!writeScore){
-            for(var i = 0; i < nodes.length; i++){
-                var thisNode = nodes[i];
-                cxt.fillRect(thisNode.x*CELL_SIZE, thisNode.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            for(i = 0; i < nodes.length; i++){
+                thisNode = nodes[i];
+                cxt.fillRect(thisNode.x*CELL_SIZE_PX, thisNode.y*CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX);
             } 
         } else {
-            for(var i = 0; i < nodes.length; i++){
+            for(i = 0; i < nodes.length; i++){
                 cxt.fillStyle = colour;
-                var thisNode = nodes[i];
-                cxt.fillRect(thisNode.x*CELL_SIZE, thisNode.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                thisNode = nodes[i];
+                cxt.fillRect(thisNode.x*CELL_SIZE_PX, thisNode.y*CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX);
                 cxt.fillStyle = TEXT_COLOUR;
-                cxt.font = "arial 15px";
-                cxt.fillText(thisNode.f, thisNode.x*CELL_SIZE, thisNode.y*CELL_SIZE+20);
+                cxt.font = SCORE_FONT;
+                cxt.fillText(thisNode.f, thisNode.x*CELL_SIZE_PX, thisNode.y*CELL_SIZE_PX+20);
             }
-        }
-    }
-
-    function fillNode(node, colour, cxt){
-        if(node.hasOwnProperty('x') && node.hasOwnProperty('y')){
-            cxt.fillStyle = colour;
-            cxt.fillRect(node.x*CELL_SIZE, node.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
     }
 
@@ -183,7 +213,7 @@ var launchPathfinder = (function(){
         for(var x = 0; x < grid.length; x++){
             for(var y = 0; y < grid[0].length; y++){
                 if(grid[x][y] == TILE_TYPE.OBSTACLE){
-                    cxt.fillRect(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                    cxt.fillRect(x*CELL_SIZE_PX, y*CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX);
                 }
             }
         }
@@ -196,8 +226,8 @@ var launchPathfinder = (function(){
             var parent = true;
             var thisNode = getNodeFromList(closed, finish);
             while(parent){
-                if(thisNode.parent != null){
-                    cxt.fillRect(thisNode.x*CELL_SIZE, thisNode.y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                if(thisNode.parent !== null){
+                    cxt.fillRect(thisNode.x*CELL_SIZE_PX, thisNode.y*CELL_SIZE_PX, CELL_SIZE_PX, CELL_SIZE_PX);
                     thisNode = thisNode.parent;
                 } else {
                     parent = false;
@@ -207,35 +237,6 @@ var launchPathfinder = (function(){
     }
 
     /** HELPER FUNCTIONS */
-    
-    function addRandomObstacles(grid){
-        var obstacleProbability = 0.05;
-        var adjacentObstacleProbability = 0.3;
-
-        //first pass puts random dots on the grid
-        for(var x = 0; x < grid.length; x++){
-            for(var y = 0; y < grid[0].length; y++){
-                if(Math.random() < obstacleProbability && grid[x][y] == TILE_TYPE.BLANK){
-                    grid[x][y] = TILE_TYPE.OBSTACLE;
-                }
-            }
-        }
-
-        //second pass beefs up the dots
-        for(var x = 0; x < grid.length; x++){
-            for(var y = 0; y < grid[0].length; y++){
-                if(grid[x][y] != TILE_TYPE.OBSTACLE && adjacentToObstacle(x,y, grid)){
-                    if(Math.random() < adjacentObstacleProbability && grid[x][y] == TILE_TYPE.BLANK){
-                        grid[x][y] = TILE_TYPE.OBSTACLE;
-                    }
-                }
-            }
-        }
-
-        return grid;
-
-    }
-
     function createNode(x, y, parent, f, g, h){
         var node = {};
         node.x = x;
@@ -274,20 +275,20 @@ var launchPathfinder = (function(){
     function isInList(list, coords){
         if(coords.hasOwnProperty('x') && coords.hasOwnProperty('y')){
             for(var i = 0; i < list.length; i++){
-                if(list[i].hasOwnProperty('x') && list[i].x == coords.x
-                && list[i].hasOwnProperty('y') && list[i].y == coords.y){
+                if(list[i].hasOwnProperty('x') && list[i].x == coords.x &&
+                 list[i].hasOwnProperty('y') && list[i].y == coords.y){
                     return true;
                 }
             }
         }
         return false;
     }
-        
+
     function getNodeFromList(list, coords){
         if(coords.hasOwnProperty('x') && coords.hasOwnProperty('y')){
             for(var i = 0; i < list.length; i++){
-                if(list[i].hasOwnProperty('x') && list[i].x == coords.x
-                && list[i].hasOwnProperty('y') && list[i].y == coords.y){
+                if(list[i].hasOwnProperty('x') && list[i].x == coords.x &&
+                 list[i].hasOwnProperty('y') && list[i].y == coords.y){
                     return list[i];
                 }
             }
@@ -314,7 +315,6 @@ var launchPathfinder = (function(){
             y < 0 || y >= grid[0].length) {
             return false;
         }
-
         return true;
     }
 
@@ -324,6 +324,6 @@ var launchPathfinder = (function(){
         point.y = Math.floor(Math.random() * maxY);
         return point;
     }
-    
+
     return init;
-})()
+})();
